30eb433860b665f8f6e59dd656ef3c413dee2f18
Виктория Внукова <C:\Users\vtaa2\AppData\Roaming\The Bat!>
2024-06-21 18:23:16 +0300

    Папки котроллеры, модели. Главное окно.

---
+++ src/main/java/module-info.java
@@ -0,0 +1,18 @@
+module dbrestaurant.dbrestaurant {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens dbrestaurant.dbrestaurant to javafx.fxml;
+    exports dbrestaurant.dbrestaurant;
+    exports dbrestaurant.dbrestaurant.controllers;
+    opens dbrestaurant.dbrestaurant.controllers to javafx.fxml;
+}
