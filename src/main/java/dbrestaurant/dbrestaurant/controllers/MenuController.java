cfa1b1bb0a70d273ec39f18c42d5b8954d2a2b8b
tarakkann <vtaa2005@gmail.com>
2024-06-21 18:38:15 +0300

    Добавление второй сцены - главное меню

---
+++ src/main/java/dbrestaurant/dbrestaurant/controllers/MenuController.java
@@ -0,0 +1,107 @@
+package dbrestaurant.dbrestaurant.controllers;
+
+import dbrestaurant.dbrestaurant.models.MenuModel;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class MenuController {
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private Button backField;
+
+    @FXML
+    private Button changeData;
+
+    @FXML
+    private Button clientsList;
+
+    @FXML
+    private Button createDish;
+
+    @FXML
+    private Button ingridientList;
+
+
+    public ResourceBundle getResources() {
+        return resources;
+    }
+
+    public void setResources(ResourceBundle resources) {
+        this.resources = resources;
+    }
+
+    public URL getLocation() {
+        return location;
+    }
+
+    public void setLocation(URL location) {
+        this.location = location;
+    }
+
+    public Button getBackField() {
+        return backField;
+    }
+
+    public void setBackField(Button backField) {
+        this.backField = backField;
+    }
+
+    public Button getChangeData() {
+        return changeData;
+    }
+
+    public void setChangeData(Button changeData) {
+        this.changeData = changeData;
+    }
+
+    public Button getClientsList() {
+        return clientsList;
+    }
+
+    public void setClientsList(Button clientsList) {
+        this.clientsList = clientsList;
+    }
+
+    public Button getCreateDish() {
+        return createDish;
+    }
+
+    public void setCreateDish(Button createDish) {
+        this.createDish = createDish;
+    }
+
+    public Button getIngridientList() {
+        return ingridientList;
+    }
+
+    public void setIngridientList(Button ingridientList) {
+        this.ingridientList = ingridientList;
+    }
+
+    @FXML
+    void initialize() {
+
+    }
+    MenuModel menuModel = new MenuModel();
+
+    @FXML
+    void switchToHelloScene(ActionEvent event) throws IOException {
+        menuModel.switchToHelloScene(event);
+
+    }
+
+    @FXML
+    void switchToDishScene(ActionEvent event) throws IOException {
+        menuModel.switchToDishScene(event);
+    }
+}
---
+++ src/main/java/dbrestaurant/dbrestaurant/models/MenuModel.java
@@ -0,0 +1,35 @@
+package dbrestaurant.dbrestaurant.models;
+
+import dbrestaurant.dbrestaurant.HelloApplication;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class MenuModel {
+    Scene scene;
+    Stage stage;
+    public void switchToHelloScene(ActionEvent event) throws IOException {
+        Parent fxmlLoader = FXMLLoader.load(HelloApplication.class.getResource("hello-view.fxml"));
+        stage = (Stage) ((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(fxmlLoader);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public void switchToDishScene(ActionEvent event) throws IOException {
+        Parent fxmlLoader = FXMLLoader.load(HelloApplication.class.getResource("dishes.fxml"));
+        stage = (Stage) ((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(fxmlLoader);
+        stage.setScene(scene);
+        stage.show();
+    }
+}
