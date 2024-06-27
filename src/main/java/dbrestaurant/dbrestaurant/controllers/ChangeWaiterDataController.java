d2f7e9a712cf1cf4374d5144af29ed21d2d9d88e
tarakkann <vtaa2005@gmail.com>
2024-06-27 23:54:35 +0300

    Новая сцена с изменением данных сотрудника текущей сессии/2

---
+++ src/main/java/dbrestaurant/dbrestaurant/controllers/ChangeWaiterDataController.java
@@ -0,0 +1,57 @@
+package dbrestaurant.dbrestaurant.controllers;
+
+import java.io.IOException;
+import java.net.URL;
+import java.sql.SQLException;
+import java.util.ResourceBundle;
+
+import dbrestaurant.dbrestaurant.SingleWrapper;
+import dbrestaurant.dbrestaurant.models.ChangeWaiterDataModel;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class ChangeWaiterDataController {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private TextField address;
+
+    @FXML
+    private Button backField;
+
+    @FXML
+    private Button changeButton;
+
+    @FXML
+    private TextField name;
+
+    @FXML
+    private TextField phone_number;
+    private final ChangeWaiterDataModel changeWaiterDataModel = new ChangeWaiterDataModel();
+
+    @FXML
+    void switchToMenuScene(ActionEvent event) throws IOException {
+        changeWaiterDataModel.switchToMenuScene(event);
+    }
+
+    @FXML
+    void initialize() {
+
+    }
+    @FXML
+    void changeData() throws SQLException, ClassNotFoundException {
+        Integer id = SingleWrapper.getInstance().getId();
+        String waiterName = name.getText();
+        String waiterAddress = address.getText();
+        String waiterPhoneNumber = phone_number.getText();
+        changeWaiterDataModel.changeData(id, waiterName, waiterAddress, waiterPhoneNumber);
+    }
+
+}
---
+++ src/main/java/dbrestaurant/dbrestaurant/models/ChangeWaiterDataModel.java
@@ -0,0 +1,40 @@
+package dbrestaurant.dbrestaurant.models;
+
+import dbrestaurant.dbrestaurant.DataConnection;
+import dbrestaurant.dbrestaurant.HelloApplication;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class ChangeWaiterDataModel {
+    private Stage stage;
+    private Scene scene;
+
+    public void switchToMenuScene(ActionEvent event) throws IOException {
+        Parent fxmlLoader = FXMLLoader.load(HelloApplication.class.getResource("menu.fxml"));
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        scene = new Scene(fxmlLoader);
+        stage.setScene(scene);
+        stage.show();
+    }
+    public void changeData(Integer id, String name, String address, String phone_number) throws SQLException, ClassNotFoundException {
+        Connection connection = DataConnection.getDBConnection();
+        String sql = "UPDATE waiters SET name = ?, address = ?, phone_number = ? WHERE id = ?";
+        PreparedStatement pst = connection.prepareStatement(sql);
+        pst.setString(1, name);
+        pst.setString(2, address);
+        pst.setString(3, phone_number);
+        pst.setInt(4, id);
+        pst.execute();
+        pst.close();
+        connection.close();
+    }
+}
